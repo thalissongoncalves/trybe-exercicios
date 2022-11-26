be4828113a0c8869b35e1749f1f25b508d3adc31
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-11-26 18:14:09 -0300

    Feito exercício 03, 04 e 05

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
@@ -16,11 +16,53 @@
 
 // Exercício 02 - Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
 
-const numero1 = 30;
-const numero2 = 50;
+// const numero1 = 30;
+// const numero2 = 50;
 
-if(numero1 > numero2) {
-    console.log("Número 30 é maior que Número 50");
-} else {
-    console.log("Número 50 é maior que Número 30");
-}
+// if(numero1 > numero2) {
+//     console.log("Número 30 é maior que Número 50");
+// } else {
+//     console.log("Número 50 é maior que Número 30");
+// }
+
+// Exercício 03 - Utilize if/else para escrever um código que retorne o maior de três números. Defina, no começo do seu código, três constantes com os valores que serão comparados.
+
+// const numero1 = 60;
+// const numero2 = 200;
+// const numero3 = 130;
+
+// if(numero1 > numero2 && numero3) {
+//     console.log("numero1 é maior que numero2 e numero3");
+// } else if(numero2 > numero3 && numero1) {
+//     console.log("numero2 é maior que numero3 e numero1");
+// } else {
+//     console.log("numero3 é maior que numero2 e numero1");
+// }
+
+// Exercício 04 - Utilize if/else para escrever um código que, dado um valor recebido como parâmetro, retorne “positive” se esse valor for positivo, “negative” se for negativo, e caso não seja nem positivo e nem negativo retorne “zero”.
+
+// const parametro = "mouse";
+
+// if (parametro === "teclado") {
+//     console.log("positive");
+// } else if (parametro === "monitor") {
+//     console.log("negative");
+// } else {
+//     console.log("0");
+// }
+
+// Exercício 05 - 🚀 Utilize if/else para escrever um código que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido, você deve retornar uma mensagem de erro.
+// Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.
+
+// Um ângulo será considerado inválido se não tiver um valor positivo.
+
+// const angulo1 = 60;
+// const angulo2 = 60;
+// const angulo3 = 60;
+
+// if(angulo1 + angulo2 + angulo3 === 180) {
+//     console.log("true, os ângulos formam um triângulo válido");
+// } else {
+//     console.log("false");
+// }
+
