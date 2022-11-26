595c9c44bf9b4e2a8e09c2d059186d836d846f4c
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-11-26 16:32:41 -0300

    Feito exercício 01 e 02 de prática

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
@@ -0,0 +1,26 @@
+// Exercício 01 - Elabore alguns códigos e imprima o resultado no console usando o console.log, um para cada operação aritmética básica. Seu código deve ter duas constantes, a e b, definidas no começo com os valores que serão operados. Escreva códigos para:
+// Adição (a + b)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
+
+// const a = 10;
+// const b = 10;
+
+// console.log("Adição: " + (a + b));
+// console.log("Subtração: " + (a - b));
+// console.log("Multiplicação: " + (a * b));
+// console.log("Divisão: " + (a / b));
+// console.log("Módulo: " + (a % b));
+
+// Exercício 02 - Utilize if/else para escrever um código que retorne o maior de dois números. Defina, no começo do seu código, duas constantes com os valores que serão comparados.
+
+const numero1 = 30;
+const numero2 = 50;
+
+if(numero1 > numero2) {
+    console.log("Número 30 é maior que Número 50");
+} else {
+    console.log("Número 50 é maior que Número 30");
+}
