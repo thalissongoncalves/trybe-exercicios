ae673e20de73fea315a9920c39e1d921fb7ab215
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-11-27 16:59:10 -0300

    Feito exercício 06 e 07

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
@@ -66,3 +66,70 @@
 //     console.log("false");
 // }
 
+//Exercício 06 - Utilize switch/case para escrever um código que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+// Como desafio, escreva um código para funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+
+// Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
+
+// Se a peça passada for inválida, o código deve retornar uma mensagem de erro.
+
+// Exemplo: bishop (bispo) -> diagonals (diagonais)
+
+// let peçaEscolhida = "rei";
+
+// switch (peçaEscolhida) {
+//     case "rei":
+//         console.log("O rei pode mover-se em todas as direções (horizontal, vertical e diagonal) somente uma casa de cada vez.");
+//         break;
+//     case "rainha":
+//         console.log("A rainha move-se ao longo da horizontal, vertical e diagonais mas não pode pular outras peças.");
+//         break;
+//     case "bispo":
+//         console.log("O bispo move-se ao longo da diagonal. Não pode pular outras peças.");
+//         break;
+//     case "cavalo":
+//         console.log("É a única peça que pode pular as outras. O movimento do cavalo é em forma de “L”, quer dizer, duas casas em sentido horizontal e mais uma na vertical ou vice-versa.");
+//         break;
+//     case "torre":
+//         console.log("A torre movimenta-se pela vertical ou horizontal, mas não pode pular outras peças.");
+//         break;
+//     case "peão":
+//         console.log("O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal. Opcionalmente, cada peão pode avançar duas casas no seu primeiro movimento do jogo.");
+//         break;
+//     default:
+//         console.log("Peça inválida.")
+// }
+
+// Exercício 07 - Utilize if/else para escrever um código que converta uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+
+// Porcentagem >= 90 -> A
+
+// Porcentagem >= 80 -> B
+
+// Porcentagem >= 70 -> C
+
+// Porcentagem >= 60 -> D
+
+// Porcentagem >= 50 -> E
+
+// Porcentagem < 50 -> F
+
+// O código deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.
+
+const nota = 1;
+
+if (nota < 0 || nota > 100) {
+    console.log("Nota inválida, insira uma nota de 0 a 100.");
+} else if (nota >= 90) {
+    console.log("A");
+} else if (nota >= 80) {
+    console.log("B");
+} else if (nota >= 70) {
+    console.log("C");
+} else if (nota >= 60) {
+    console.log("D");
+} else if (nota >= 50) {
+    console.log("E");
+} else {
+    console.log("F");
+}
