9e698b45a72c7eabde11d638bbf6d644b9ca3381
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-11-27 19:36:30 -0300

    Feito exercicio 09

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
@@ -137,13 +137,27 @@
 // Exercício 08 - Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for par. Caso contrário, ele deve retornar false.
 // Bonus: use somente um if.
 
-const number1 = 13;
-const number2 = 15;
+// const number1 = 13;
+// const number2 = 15;
+// const number3 = 12;
+
+// let isEven = false;
+
+// if(number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
+//     isEven = true;
+// }
+// console.log(isEven);
+
+// Exercício 09 - Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
+// Bonus: use somente um if.
+
+const number1 = 10;
+const number2 = 13;
 const number3 = 12;
 
-let isEven = false;
+let isOdd = false;
 
-if(number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 === 0) {
-    isEven = true;
+if(number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0) {
+    isOdd = true;
 }
-console.log(isEven);
+console.log(isOdd);
