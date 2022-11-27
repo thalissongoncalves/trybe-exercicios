1cd757857a4a36d7400ba327eddbc62a439c5a47
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-11-27 20:04:33 -0300

    Feito exercicio 10

--- fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-01-javscript-primeiros-passos/Exercícios - agora, a prática.js
@@ -151,13 +151,34 @@
 // Exercício 09 - Utilize if/else para escrever um código que defina três números em constantes e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele deve retornar false.
 // Bonus: use somente um if.
 
-const number1 = 10;
-const number2 = 13;
-const number3 = 12;
+// const number1 = 10;
+// const number2 = 13;
+// const number3 = 12;
 
-let isOdd = false;
+// let isOdd = false;
 
-if(number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0) {
-    isOdd = true;
+// if(number1 % 2 !== 0 || number2 % 2 !== 0 || number3 % 2 !== 0) {
+//     isOdd = true;
+// }
+// console.log(isOdd);
+
+// Exercício 10 - Utilize if/else para escrever um código que se inicie com dois valores em duas constantes diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda, descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+// Atente que, sobre o custo do produto, incide um imposto de 20%.
+
+// Seu código também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+
+// O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+
+// valorCustoTotal = valorCusto + impostoSobreOCusto;
+// lucro = valorVenda - valorCustoTotal (lucro de um produto);
+
+const costOfProduct = 2;
+const saleValue = 5;
+
+if (costOfProduct >= 0 && saleValue >= 0) {
+    const totalCostOfProduct = costOfProduct * 1.2;
+    const totalProfit = (saleValue - totalCostOfProduct) * 1000;
+    console.log(totalProfit);
+} else {
+    console.log("Erro, os valores não podem ser negativos.")
 }
-console.log(isOdd);
