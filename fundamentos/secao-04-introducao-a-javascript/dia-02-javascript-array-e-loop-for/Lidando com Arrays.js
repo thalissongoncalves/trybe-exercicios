893ddd9a15337f61d5ce71ab1dea0c0e298693fc
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-11-30 18:34:06 -0300

    Feito exercício 01, 02, 03 - Lidando com Arrays

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-02-javascript-array-e-loop-for/Lidando com Arrays.js
@@ -0,0 +1,27 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//Exercício 01: Percorra o array imprimindo todos os valores nele contidos com a função console.log();
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//     console.log(numbers[index]);
+// }
+
+//Exercício 02: Some todos os valores contidos no array e imprima o resultado;
+
+// let sum = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+// }
+
+// console.log(sum);
+
+//Exercício 03: Calcule e imprima a média aritmética dos valores contidos no array;
+
+// A média aritmética é o resultado da soma de todos os elementos dividido pelo número total de elementos.
+
+// let sum = 0;
+// for (let index = 0; index < numbers.length; index += 1) {
+//     sum += numbers[index];
+// }
+
+// console.log(sum / numbers.length);
