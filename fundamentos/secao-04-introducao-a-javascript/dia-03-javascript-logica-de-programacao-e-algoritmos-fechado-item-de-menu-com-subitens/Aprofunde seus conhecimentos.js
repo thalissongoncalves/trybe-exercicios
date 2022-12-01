0b0b5e6cbf44fb23ece9af8fbbdbddd2c8fda58a
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-01 20:27:38 -0300

    Corrigido exercício 02

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Aprofunde seus conhecimentos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Aprofunde seus conhecimentos.js
@@ -1,39 +1,38 @@
 //Exercício 01 - Com base nessas informações, crie um algoritmo que imprima na tela o fatorial de 10.
 
-let fatorial = 1;
+// let fatorial = 1;
 
-for (let index = 10; index > 0; index -= 1) {
-    fatorial *= index;
-}
+// for (let index = 10; index > 0; index -= 1) {
+//     fatorial *= index;
+// }
 
-console.log(fatorial);
+// console.log(fatorial);
 
 //Exercício 02 - Utilize a estrutura de repetição for para desenvolver um algoritmo que seja capaz de inverter uma palavra. Por exemplo, a palavra “banana” seria invertida para “ananab”. Utilize a string abaixo como exemplo, depois troque por outras para verificar se seu algoritmo está funcionando corretamente.
 
-// const solucao2 = str => {
-// let reversed = '';
+// let word = 'tryber';
+// let reverseWord = '';
 
-// for(let character of str) {
-//     reversed = character + reversed;
-//   }
-//   return reversed;
+// for (let index = 0; index < word.length; index += 1) {
+//     reverseWord += word[word.length - 1 - index];
 // }
-// console.log(solucao2('tryber'));
+
+// console.log(reverseWord);
 
 //Exercício 03 - Considere o array de strings abaixo:
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
 // Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
 
-let biggestWord = '';
-let smallestWord = '';
+// let biggestWord = '';
+// let smallestWord = '';
 
-for (let index = 0; index < array.length; index += 1) {
-    biggestWord = array[index];
-    if (biggestWord > array[index]) {
-        console.log(biggestWord);
-    }
-    smallestWord = array[index];
-    if (smallestWord < array) {
-        console.log(smallestWord);
-    }
-}
+// for (let index = 0; index < array.length; index += 1) {
+//     biggestWord = array[index];
+//     if (biggestWord > array[index]) {
+//         console.log(biggestWord);
+//     }
+//     smallestWord = array[index];
+//     if (smallestWord < array) {
+//         console.log(smallestWord);
+//     }
+// }
