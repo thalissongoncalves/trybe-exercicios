905da1994eea4806489359168ba43f7a4d2757d5
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-01 21:45:59 -0300

    Feito exercício BÔNUS 02

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Exercícios Bônus.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Exercícios Bônus.js
@@ -8,13 +8,33 @@
 // *****
 // *****
 
+// let n = 5;
+// let symbol = '*';
+// let inputLine = '';
+
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//     inputLine = inputLine + symbol;
+// };
+// for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+//     console.log(inputLine);
+// };
+
+
+// Exercício 02 - Faça o mesmo que antes, mas que imprima um triângulo retângulo com 5 asteriscos de base. Por exemplo:
+
+// n = 5
+
+// *
+// **
+// ***
+// ****
+// *****
+
 let n = 5;
 let symbol = '*';
 let inputLine = '';
 
 for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
     inputLine = inputLine + symbol;
-};
-for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
     console.log(inputLine);
 };
