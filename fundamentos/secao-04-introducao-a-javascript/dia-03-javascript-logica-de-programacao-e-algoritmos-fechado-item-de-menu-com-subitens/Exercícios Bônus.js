383514454a4a8d6b0e35f97b81ddc6fdc84a398c
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-01 21:42:25 -0300

    Feito exercício BÔNUS 01

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Exercícios Bônus.js
@@ -0,0 +1,20 @@
+// Exercício 01 - Escreva um programa que, dado um valor n qualquer, seja n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n. Por exemplo:
+
+// n = 5
+
+// *****
+// *****
+// *****
+// *****
+// *****
+
+let n = 5;
+let symbol = '*';
+let inputLine = '';
+
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    inputLine = inputLine + symbol;
+};
+for (let lineIndex = 0; lineIndex < n; lineIndex += 1) {
+    console.log(inputLine);
+};
