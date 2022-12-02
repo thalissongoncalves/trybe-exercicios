20b725d5f1e315bde3922e3c03bf24c8dd4390ee
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-01 21:29:25 -0300

    Feito exercício 04

--- fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Aprofunde seus conhecimentos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-03-javascript-logica-de-programacao-e-algoritmos-fechado-item-de-menu-com-subitens/Aprofunde seus conhecimentos.js
@@ -21,24 +21,42 @@
 
 //Exercício 03 - Considere o array de strings abaixo:
 
-let array = ['java', 'javascript', 'python', 'html', 'css'];
+// let array = ['java', 'javascript', 'python', 'html', 'css'];
 
 // Utilize a estrutura de repetição for para escrever dois algoritmos: um que imprima no console a maior palavra desse array e outro que imprima a menor. Considere o número de caracteres de cada palavra.
 
-let biggestWord = array[0];
-let smallestWord = array[0];
+// let biggestWord = array[0];
+// let smallestWord = array[0];
 
-for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length > biggestWord.length) {
-        biggestWord = array[index];
+// for (let index = 0; index < array.length; index += 1) {
+//     if (array[index].length > biggestWord.length) {
+//         biggestWord = array[index];
+//     }
+// }
+
+// for (let index = 0; index < array.length; index += 1) {
+//     if (array[index].length < smallestWord.length) {
+//         smallestWord = array[index];
+//     }
+// }
+
+// console.log(biggestWord);
+// console.log(smallestWord);
+
+//Exercício 04 - Um número primo é um número inteiro maior do que 1 que possui somente dois divisores, ou seja, é divisível por 1 e por ele mesmo. Sabendo disso, escreva um algoritmo que imprima no console o maior número primo entre 2 e 50.
+
+let biggestPrimeNumber = 0;
+
+for (let currentNumber = 2; currentNumber <= 50; currentNumber += 1) {
+    let isPrime = true;
+    for (let currentDivisor = 2; currentDivisor < currentNumber; currentDivisor += 1) {
+        if (currentNumber % currentDivisor === 0) {
+            isPrime = false;
+        }
+    }
+    if (isPrime) {
+        biggestPrimeNumber = currentNumber;
     }
 }
 
-for (let index = 0; index < array.length; index += 1) {
-    if (array[index].length < smallestWord.length) {
-        smallestWord = array[index];
-    }
-}
-
-console.log(biggestWord);
-console.log(smallestWord);
+console.log(biggestPrimeNumber);
