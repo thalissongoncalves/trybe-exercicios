ba70127dada95d03437218b85e77acfd5f0bf1c5
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-03 20:19:54 -0300

    Feito exercício 01 - Para fixar - for/in e for/of

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Objetos.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Objetos.js
@@ -1,4 +1,5 @@
-// Para fixar: Exercício 01 - Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
+// Para fixar: 
+// Exercício 01 - Crie uma variável player e atribua um objeto contendo as variáveis listadas abaixo:
 
 // let name = 'Marta';
 // let lastName = 'Silva';
