ed98b9d0014153575ace8a50d08ce3c37c4d6fad
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 15:25:10 -0300

    Feito exercício 04 - tópico Exercícios, objetos e for/in

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
@@ -34,3 +34,14 @@ console.log(info);
 for (let index in info) {
     console.log(index);
 }
+
+// Exercício 04 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+for (let index in info) {
+    console.log(info[index]);
+}
