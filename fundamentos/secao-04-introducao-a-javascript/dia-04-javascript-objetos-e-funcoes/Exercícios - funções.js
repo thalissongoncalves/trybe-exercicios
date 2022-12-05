2677aec3b8fb1259ceb10078a0a7e4e740fd0ef7
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 20:49:06 -0300

    Feito exercício 07 - tópico Exercícios, funções

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
@@ -123,3 +123,31 @@ function somatorio (N) {
 
 console.log(somatorio(5));
 
+// Exercício 07 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. Considere que a string ending sempre será menor que a string word.
+
+// Valor de teste: 'trybe' e 'be'
+
+// Valor esperado no retorno da função: true
+
+// verificaFimPalavra('trybe', 'be');
+
+// Retorno esperado: true
+// verificaFimPalavra('joaofernando', 'fernan');
+
+// Retorno esperado: false
+
+function checkWord (word, ending) {
+    word = word.split('');
+    ending = ending.split('');
+    let control = true;
+
+    for (let index = 0; index < ending.length; index += 1) {
+        if (word[word.length - ending.length + index] != ending[index]) {
+            control = false;
+        }
+    }
+    return control;
+}
+
+console.log(checkWord('Thálisson', 'ssan'));
+console.log(checkWord('Thálisson', 'sson'));
