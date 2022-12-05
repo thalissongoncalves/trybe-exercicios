443c6afef9159aa8922351ef997a60fcddd19543
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 15:54:22 -0300

    Feito exercício 06 - tópico Exercícios, objetos e for/in

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
@@ -1,16 +1,16 @@
 // Usando o objeto abaixo, faça os exercícios a seguir:
 
-let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+// let info = {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   };
 
 // Exercício 01 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Use a sintaxe meuObjeto.chave. Valor esperado no console:
 
 // Bem-vinda, Margarida
 
-    console.log('Bem-vinda, ' + info.personagem);
+    // console.log('Bem-vinda, ' + info.personagem);
 
 // Exercício 02 - Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
 
@@ -21,8 +21,8 @@ let info = {
 //     recorrente: 'Sim'
 //   }
 
-info['recorrente'] = 'Sim';
-console.log(info);
+// info['recorrente'] = 'Sim';
+// console.log(info);
 
 // Exercício 03 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
 
@@ -31,9 +31,9 @@ console.log(info);
 // nota
 // recorrente
 
-for (let index in info) {
-    console.log(index);
-}
+// for (let index in info) {
+//     console.log(index);
+// }
 
 // Exercício 04 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
 
@@ -53,21 +53,41 @@ for (let index in info) {
 // Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
 // Ambos recorrentes //// Atenção para essa última linha!
 
-let info2 = {
-    personagem: ' e Tio Patinhas',
-    origem: ' e Christmas on Bear Mountain, Dell´s Four Color Comics #178',
-    nota: ' e O último MacPatinhas',
-    recorrente: 'Sim',
-}
+// let info2 = {
+//     personagem: ' e Tio Patinhas',
+//     origem: ' e Christmas on Bear Mountain, Dell´s Four Color Comics #178',
+//     nota: ' e O último MacPatinhas',
+//     recorrente: 'Sim',
+// }
 
-for (let properties in info) {
-    if (
-        properties === 'recorrente' &&
-        info[properties] === 'Sim' &&
-        info2[properties] === 'Sim'
-    ) {
-        console.log('Ambos recorrentes');
-    } else {
-        console.log(info[properties] + ' e ' + info2[properties]);
-    }
-}
+// for (let properties in info) {
+//     if (
+//         properties === 'recorrente' &&
+//         info[properties] === 'Sim' &&
+//         info2[properties] === 'Sim'
+//     ) {
+//         console.log('Ambos recorrentes');
+//     } else {
+//         console.log(info[properties] + ' e ' + info2[properties]);
+//     }
+// }
+
+// 🚀 Leitura de Objetos
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+
+// Exercício 06 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: “O livro favorito de Julia Pessoa se chama ‘O Pior Dia de Todos’”.
+
+console.log('O livro favorito de ' + leitor.nome + ' ' + leitor.sobrenome + ' se chama "' + leitor.livrosFavoritos[0].titulo + '".');
