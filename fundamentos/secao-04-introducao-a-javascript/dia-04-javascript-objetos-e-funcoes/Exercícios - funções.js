94af4f92f278ce22aa50dd6cbbcb35a7ff163dba
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 19:25:23 -0300

    Feito exercício 04 - tópico Exercícios, funções

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
@@ -58,3 +58,21 @@ function minorIndex (numbers) {
 }
 
 console.log(minorIndex([2, 4, 6, 7, 10, 1, 3]));
+
+// Exercício 04 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+// Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
+
+// Valor esperado no retorno da função: Fernanda.
+
+function indexWithMostCharacters (strings) {
+    let indexWithMostCharacters = strings[0];
+    for (let index in strings) {
+        if (indexWithMostCharacters.length < strings[index].length)
+        indexWithMostCharacters = strings[index];
+    }
+
+    return indexWithMostCharacters;
+}
+
+console.log(indexWithMostCharacters(['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana']));
