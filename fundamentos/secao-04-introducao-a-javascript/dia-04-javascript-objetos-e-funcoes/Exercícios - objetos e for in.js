325e56495e50de8f9b96edc92660d29be99103c0
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 15:41:46 -0300

    Feito exercício 05 - tópico Exercícios, objetos e for/in

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
@@ -42,6 +42,32 @@ for (let index in info) {
 // Namorada do personagem principal nos quadrinhos do Pato Donald
 // Sim
 
-for (let index in info) {
-    console.log(info[index]);
+// for (let index in info) {
+//     console.log(info[index]);
+// }
+
+// Exercício 05 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: ‘Tio Patinhas’, ‘Christmas on Bear Mountain, Dell’s Four Color Comics #178’, ‘O último MacPatinhas’, ‘Sim’. Então, imprima os valores de cada objeto juntos, de acordo com cada uma das chaves. Valor esperado no console:
+
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes //// Atenção para essa última linha!
+
+let info2 = {
+    personagem: ' e Tio Patinhas',
+    origem: ' e Christmas on Bear Mountain, Dell´s Four Color Comics #178',
+    nota: ' e O último MacPatinhas',
+    recorrente: 'Sim',
+}
+
+for (let properties in info) {
+    if (
+        properties === 'recorrente' &&
+        info[properties] === 'Sim' &&
+        info2[properties] === 'Sim'
+    ) {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[properties] + ' e ' + info2[properties]);
+    }
 }
