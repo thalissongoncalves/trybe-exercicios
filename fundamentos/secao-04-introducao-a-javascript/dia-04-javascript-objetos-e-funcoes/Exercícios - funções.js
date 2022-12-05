7594db7f281323a3574423343e631105e648118b
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 18:28:16 -0300

    Feito exercício 01 - tópico Exercícios, funções

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
@@ -0,0 +1,20 @@
+// Exercício 01 - Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
+// Exemplo de palíndromo: arara
+
+// verificaPalindrome('arara')
+
+// Retorno esperado: true
+// verificaPalindrome('desenvolvimento')
+
+// Retorno esperado: false
+
+function palindromo (palavra) {
+    if (palavra === 'ovo') {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+console.log(palindromo('ova'));
