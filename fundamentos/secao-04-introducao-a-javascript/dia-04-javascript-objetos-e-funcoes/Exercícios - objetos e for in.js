3be34649b169d26748b4a6ba74061709a4565088
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 15:22:13 -0300

    Feito exercício 02 - tópico Exercícios, objetos e for/in

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - objetos e for in.js
@@ -11,3 +11,15 @@ let info = {
 // Bem-vinda, Margarida
 
     console.log('Bem-vinda, ' + info.personagem);
+
+// Exercício 02 - Insira no objeto uma nova propriedade com o nome de chave ‘recorrente’ e o valor ‘Sim’ e, em seguida, imprima o objeto no console. Use a sintaxe meuObjeto['chave'] = valor. Valor esperado no console:
+
+// {
+//     personagem: 'Margarida',
+//     origem: 'Pato Donald',
+//     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//     recorrente: 'Sim'
+//   }
+
+info['recorrente'] = 'Sim';
+console.log(info);
