7e5416660f3a8427a35d48ba23c4e1c6df1ca709
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-05 20:23:11 -0300

    Feito exercício 06 - tópico Exercícios, funções

--- fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-04-javascript-objetos-e-funcoes/Exercícios - funções.js
@@ -106,3 +106,20 @@ function mostRepeatedNumber (numbers) {
 }
 
 console.log(mostRepeatedNumber([4, 3, 4, 5, 8, 4, 3]));
+
+// Exercício 06 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+// Valor de teste: N = 5.
+
+// Valor esperado no retorno da função: 1+2+3+4+5 = 15.
+
+function somatorio (N) {
+    let total = 0;
+    for (let index = 1; index <= N; index += 1) {
+        total = total + index;
+    }
+    return total;
+}
+
+console.log(somatorio(5));
+
