c8308044b8da2dd752d3aa749bdb3b39b1b5e4c6
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-06 20:36:09 -0300

    Feito exercício 04 - tópico, exercícios agora a pratica

--- fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/Exercícios - Agora a prática.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/Exercícios - Agora a prática.js
@@ -5,37 +5,54 @@
 
 // Copie o código abaixo e rode-o para verificar sua saída:
 
-function imprimeIdade() {
-    for (let idade = 30; idade <= 40; idade += 1) {
-      console.log('Idade dentro do for:', idade)
-    }
+// function imprimeIdade() {
+//     for (let idade = 30; idade <= 40; idade += 1) {
+//       console.log('Idade dentro do for:', idade)
+//     }
     
-  }
-  imprimeIdade()
+//   }
+//   imprimeIdade()
 
 // Exercício 02
 
 // 🚀 Altere o valor das propriedades do objeto, para que respeite as características da variável do tipo const;
 // Copie o código abaixo e rode-o para verificar sua saída:
 
-  const pessoa = {
-    nome: 'Henri',
-    idade: 20
-  }
-  pessoa.nome = {
-    nome: 'Luna'
-  }
-  pessoa.idade = {
-    idade: 19
-  } 
-  console.log('Nome:', pessoa.nome);
-  console.log('Idade:', pessoa.idade);
+//   const pessoa = {
+//     nome: 'Henri',
+//     idade: 20
+//   }
+//   pessoa.nome = {
+//     nome: 'Luna'
+//   }
+//   pessoa.idade = {
+//     idade: 19
+//   } 
+//   console.log('Nome:', pessoa.nome);
+//   console.log('Idade:', pessoa.idade);
 
 // Exercício 03
 
 // 🚀 Modifique a variável para que não ocorra Erro;
 // Copie o código abaixo e rode-o para verificar sua saída:
 
-const favoriteFood = {comida: 'Lasanha'};
-favoriteFood.comida = 'Hamburguer';
-console.log('Comida Favorita:', favoriteFood.comida);
+// const favoriteFood = {comida: 'Lasanha'};
+// favoriteFood.comida = 'Hamburguer';
+// console.log('Comida Favorita:', favoriteFood.comida);
+
+// Exercício 04
+
+// Modifique as concatenações para template literals.
+
+// Copie o código abaixo:
+
+const name = 'Adriana';
+const lastName = 'Soares';
+console.log('Olá' + ',' + name + ' ' + lastName + '!');
+function soma(a,b) {
+  let resultado = a + b;
+  return resultado;
+}
+let a = 3;
+let b = 5;
+console.log(`O resultado da soma de ${a} + ${b} é: ${soma(a,b)}`);
