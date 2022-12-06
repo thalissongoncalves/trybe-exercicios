45245024e4f26e35cbd5edc637509e40a17dff67
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-06 20:19:21 -0300

    Feito exercício 01 - tópico Exercícios, agora a pratica

---
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/Exercícios - Agora a prática.js
@@ -0,0 +1,14 @@
+// Exercício 01
+// 🚀 Altere o tipo das variáveis (var) para let ou const para que respeitem o escopo em que foram declaradas.
+
+// 🚀 Modifique a variável para que respeite o escopo onde está sendo declarada. Lembre-se: para que a variável respeite o escopo, ela não deve ser acessível fora do escopo em que esteja sendo declarada.
+
+// Copie o código abaixo e rode-o para verificar sua saída:
+
+function imprimeIdade() {
+    for (let idade = 30; idade <= 40; idade += 1) {
+      console.log('Idade dentro do for:', idade)
+    }
+    
+  }
+  imprimeIdade()
