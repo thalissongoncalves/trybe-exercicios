90ccaf1cce91386534e8589f1e54419443771bbc
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-08 15:22:26 -0300

    Feito exercício do tópico Qual seletor devo usar

---
+++ fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-01-javascript-DOM-e-seletores/Qual seletor devo usar/script.js
@@ -0,0 +1,29 @@
+const h1 = document.querySelectorAll('#header-container')[0];
+h1.style.background = '#00B069';
+
+const titlediv1 = document.querySelector('.emergency-tasks div h3');
+titlediv1.style.background = '#A500F3';
+
+const titlediv2 = document.querySelectorAll('.emergency-tasks div h3')[1];
+titlediv2.style.background = '#A500F3';
+
+const div1 = document.querySelector('.emergency-tasks div');
+div1.style.background = '#FF9F84';
+
+const div2 = document.querySelectorAll('.emergency-tasks div')[1];
+div2.style.background = '#FF9F84';
+
+const titlediv3 = document.querySelector('.no-emergency-tasks div h3');
+titlediv3.style.background = 'black';
+
+const titlediv4 = document.querySelectorAll('.no-emergency-tasks div h3')[1];
+titlediv4.style.background = 'black';
+
+const div3 = document.querySelector('.no-emergency-tasks div');
+div3.style.background = '#F9DB5E';
+
+const div4 = document.querySelectorAll('.no-emergency-tasks div')[1];
+div4.style.background = '#F9DB5E';
+
+const footer = document.querySelector('#footer-container');
+footer.style.background = '#003533';
