696a4f1dd521d792d55fb49a9bd95513a1f99563
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2022-12-07 21:25:41 -0300

    Feito exercício 03 - tópico Exercícios, Mais prática

--- fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/Exercícios - Mais prática.js
+++ fundamentos/secao-04-introducao-a-javascript/dia-05-javascript-es6-let-const-arrow-functions-e-template-literals/Exercícios - Mais prática.js
@@ -65,3 +65,36 @@ const circleArea = (raio) => {
 }
 
 console.log(`Essa é a área do círculo: ${circleArea(5)}`)
+
+// Exercício 03
+
+// 🚀 Crie uma função que receba uma frase como parâmetro e retorne a maior palavra da frase.
+
+// Exemplo: longestWord('Antônio foi no banheiro e não sabemos o que aconteceu') // retorna 'aconteceu'
+
+// De olho na dica 👀: Nesse exercício, será necessário utilizar os conhecimentos sobre array, método split e loop for/of.
+
+// Bônus (opcional): Tente fazer o mesmo exercício utilizando o método array.sort().
+
+const maiorPalavra = (frase) => {
+    const wordArray = frase.split(' ');
+    let maxLength = 0;
+    let result = '';
+
+    for (const word of wordArray) {
+        if (word.length > maxLength) {
+            maxLength = word.length;
+            result = word;
+        }
+    }
+
+    return result;
+}
+
+console.log(maiorPalavra('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
+// OU
+
+const longestWord = text => text.split(' ').sort((wordA, wordB) => wordB.length - wordA.length)[0];
+
+console.log(longestWord("Antonio foi ao banheiro e não sabemos o que aconteceu"));
