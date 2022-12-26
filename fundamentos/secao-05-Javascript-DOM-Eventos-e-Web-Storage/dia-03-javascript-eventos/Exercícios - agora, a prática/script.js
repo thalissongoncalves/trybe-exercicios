c194dbdd80668e8d32ecd7cf19e25b8a5a7d0f57
Thálisson Gonçalves <118013962+thalissongoncalves@users.noreply.github.com>
2022-12-26 07:20:08 +0000

    Feito exercício 08 do tópico Exercícios - agora, a prática

--- fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-03-javascript-eventos/Exercícios - agora, a prática/script.js
+++ fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-03-javascript-eventos/Exercícios - agora, a prática/script.js
@@ -174,3 +174,19 @@ const addTaskToList = (taskName) => {
 }
 
 addTaskToList('Cozinhar');
+
+// Parte 8
+// Implemente uma função que adicione uma legenda com cor para a tarefa;
+// Essa função deverá receber como parâmetro uma string (‘cor’) e criar dinamicamente um elemento de tag <div> com a classe task;
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada;
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+const backgroundColorTasks = (color) => {
+  const myTasks = document.querySelector('.my-tasks');
+  const divBackground = document.createElement('div');
+  divBackground.style.backgroundColor = color;
+  divBackground.className = 'task';
+  myTasks.appendChild(divBackground);
+}
+
+backgroundColorTasks('green');
