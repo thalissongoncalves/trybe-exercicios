f791da8f61e1f5652141427b8122fbba95b3ed97
Thálisson Gonçalves <118013962+thalissongoncalves@users.noreply.github.com>
2022-12-26 07:12:10 +0000

    Feito exercício 07 do tópico Exercícios - agora, a prática

--- fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-03-javascript-eventos/Exercícios - agora, a prática/script.js
+++ fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-03-javascript-eventos/Exercícios - agora, a prática/script.js
@@ -160,3 +160,17 @@ const dayMouseOut = () => {
 
 dayMouseOver();
 dayMouseOut();
+
+// Parte 7
+// Implemente uma função que adicione uma tarefa personalizada ao calendário;
+// A função deve receber como parâmetro a string com o nome da tarefa (ex: “cozinhar”) e criar dinamicamente um elemento com a tag <span> contendo a tarefa;
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+
+const addTaskToList = (taskName) => {
+  const myTasks = document.querySelector('.my-tasks');
+  const myTasksChild = document.createElement('span');
+  myTasksChild.innerHTML = taskName;
+  myTasks.appendChild(myTasksChild);
+}
+
+addTaskToList('Cozinhar');
