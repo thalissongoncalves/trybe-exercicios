49cc309dcd52bd02b1647294c3877d205fe7d871
Thálisson Gonçalves <118013962+thalissongoncalves@users.noreply.github.com>
2022-12-28 03:34:31 +0000

    Adicionado conteúdo a página e adicionado borda a div responsabilizada pelos botões de personalização da página para o usuário

---
+++ fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-04-javascript-web-storage/script.js
@@ -0,0 +1,9 @@
+const body = document.getElementById('body');
+body.style.textAlign = 'center';
+
+const border = document.querySelector('.border');
+border.style.border = '1px';
+border.style.borderStyle = 'solid';
+border.style.borderColor = 'green';
+border.style.height = '200px';
+border.style.Width = '100%';
