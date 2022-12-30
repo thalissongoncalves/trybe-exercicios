9629d766829982f3b7b66a5bbe94e562a44799be
Thálisson Gonçalves <118013962+thalissongoncalves@users.noreply.github.com>
2022-12-30 06:44:45 +0000

    Adicionado mais uma lista de seleção para alteração de cor da fonte de texto

--- fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-04-javascript-web-storage/script.js
+++ fundamentos/secao-05-Javascript-DOM-Eventos-e-Web-Storage/dia-04-javascript-web-storage/script.js
@@ -1,7 +1,26 @@
 window.onload = function () {
-  let select = document.querySelector('select');
-  select.addEventListener('change', () => {
-      let selected = select.selectedOptions[0];
-      document.body.style.backgroundColor = selected.value;
+
+  let corDeFundo = document.querySelector('#cordefundo');
+  corDeFundo.addEventListener('change', () => {
+    let selected = corDeFundo.selectedOptions[0];
+    document.body.style.backgroundColor = selected.value;
+
+    localStorage.setItem('5.4-background-color', selected.value);
+  })
+
+  let corDeTexto = document.querySelector('#cordetexto');
+  corDeTexto.addEventListener('change', () => {
+    let selected = corDeTexto.selectedOptions[0];
+    document.body.style.color = selected.value;
+
+    localStorage.setItem('5.4-font-color', selected.value);
+  })
+
+  let inputFontSize = document.querySelector('input[name="font-size"]')
+  inputFontSize.addEventListener('change', () => {
+    let paragraph = document.querySelector('p');
+    paragraph.style.fontSize = `${inputFontSize.value}px`;
+
+    localStorage.setItem('5.4-font-size', `${inputFontSize.value}px`);
   })
 }
