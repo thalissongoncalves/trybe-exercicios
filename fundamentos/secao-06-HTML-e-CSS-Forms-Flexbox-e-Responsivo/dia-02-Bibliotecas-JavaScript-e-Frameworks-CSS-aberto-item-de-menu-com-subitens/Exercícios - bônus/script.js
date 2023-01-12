026f211bc3bb781b4ae46b90df854c0ae9192ef0
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-01-12 17:12:24 -0300

    Feito exercícios - bônus

---
+++ fundamentos/secao-06-HTML-e-CSS-Forms-Flexbox-e-Responsivo/dia-02-Bibliotecas-JavaScript-e-Frameworks-CSS-aberto-item-de-menu-com-subitens/Exercícios - bônus/script.js
@@ -0,0 +1,33 @@
+$(function() {
+    $( "#date" ).datepicker({
+        showOn: "button",
+        buttonImage: "calendario.png",
+        buttonImageOnly: true,
+        showButtonPanel:true
+    });
+});
+
+const validate = new window.JustValidate('#form');
+const validation = new JustValidate('#form');
+
+validation
+  .addField('#name', [
+    {
+      rule: 'minLength',
+      value: 3,
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+    },
+  ])
+  .addField('#email', [
+    {
+      rule: 'required',
+      errorMessage: 'Email is required',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Email is invalid!',
+    },
+  ]);
