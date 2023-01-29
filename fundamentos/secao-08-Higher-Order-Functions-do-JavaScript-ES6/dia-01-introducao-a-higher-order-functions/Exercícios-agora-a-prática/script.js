c3d5cd8ae2aac1059436d5669891dbe23f86f5c9
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-01-29 20:53:17 -0300

    Feito exercício 02 do tópico Exercícios - agora, a prática

--- fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-introducao-a-higher-order-functions/Exercícios-agora-a-prática/script.js
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-introducao-a-higher-order-functions/Exercícios-agora-a-prática/script.js
@@ -22,3 +22,22 @@ const generateEmployees = (fullName) => {
 }
 
 console.log(newEmployees(generateEmployees));
+
+// Sorteador de loteria
+// Desenvolva uma HOF que retorne o resultado de um sorteio. Essa HOF irá gerar um número aleatório entre 1 e 5, recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns, você ganhou”).
+
+const numberChecker = (myNumber, number) => { 
+    if(myNumber === number) {
+        return "Parabéns, você ganhou!";
+    } else {
+        return "Tente novamente.";
+    }
+}
+
+const numberGenerator = () => {
+    const saveNumber = Math.random() * 5;
+    const roundNumber = parseInt(saveNumber);
+    return roundNumber;
+};
+
+console.log(numberChecker(1, numberGenerator()));
