ef542f8245e7f88479d3047fc68005d5b7c4aba4
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-01-30 04:08:43 -0300

    Feito exercicio 03 do tópico Exercícios - agora, a prática

--- fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-introducao-a-higher-order-functions/Exercícios-agora-a-prática/script.js
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-01-introducao-a-higher-order-functions/Exercícios-agora-a-prática/script.js
@@ -26,18 +26,47 @@ console.log(newEmployees(generateEmployees));
 // Sorteador de loteria
 // Desenvolva uma HOF que retorne o resultado de um sorteio. Essa HOF irá gerar um número aleatório entre 1 e 5, recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: “Tente novamente” ou “Parabéns, você ganhou”).
 
-const numberChecker = (myNumber, number) => { 
-    if(myNumber === number) {
-        return "Parabéns, você ganhou!";
-    } else {
-        return "Tente novamente.";
-    }
-}
+const numberChecker = (myNumber, number) => myNumber === number;
 
-const numberGenerator = () => {
+const numberGenerator = (myNumber, callback) => {
     const saveNumber = Math.random() * 5;
-    const roundNumber = parseInt(saveNumber);
-    return roundNumber;
+    const number = parseInt(saveNumber);
+
+    return callback(myNumber, number) ? 'Parabéns, você ganhou!' : 'Tente novamente.';
 };
 
-console.log(numberChecker(1, numberGenerator()));
+console.log(numberGenerator(1, numberChecker));
+
+// Corretor automático de exame
+// Crie uma HOF que receberá três parâmetros:
+// O primeiro será um array de respostas corretas (soluções);
+// O segundo será um array contendo as respostas fornecidas por uma pessoa estudante;
+// O terceiro é uma função que compara os dois arrays e então dá uma pontuação baseada nos acertos. Para isso, essa função usará os seguintes critérios:
+// Uma resposta correta adiciona 1 ponto à pontuação final;
+// A ausência de uma resposta não altera a pontuação (quando for “N.A”);
+// Uma resposta incorreta reduz a pontuação final em 0.5 ponto.
+// Ao final, a HOF deve retornar o total de pontos obtidos através das respostas fornecidas pela pessoa estudante. Utilize os seguintes arrays:
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compareAnswers = (rightAnswer, studentAnswer) => {
+    if (rightAnswer === studentAnswer) {
+        return 1;
+    }
+    if (studentAnswer === 'N.A') {
+        return 0;
+    }
+    return -0.5;
+}
+
+const countPoints = (rightAnswers, studentAnswers, action) => {
+    let contador = 0;
+    for (let index = 0; index < rightAnswers.length; index += 1) {
+        const actionReturn = action(rightAnswers[index], studentAnswers[index]);
+        contador += actionReturn;
+    }
+    return `Resultado final: ${contador} pontos`;
+};
+  
+console.log(countPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, compareAnswers));
