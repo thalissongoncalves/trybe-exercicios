cec5610048953ada10c3094d0c12bade9618e05f
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-01-31 19:28:10 -0300

    Feito exercício 03 da Parte 1 do tópico Exercícios - agora, a prática

--- fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-03-Higher-Order-Functions-filter-e-reduce/Exercícios - agora, a prática/Parte 1/exercise3.js
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-03-Higher-Order-Functions-filter-e-reduce/Exercícios - agora, a prática/Parte 1/exercise3.js
@@ -62,3 +62,12 @@ const books = [
   ];
 
 // 3 - Faça uma função que retorne os nomes dos livros, dado o ano de nascimento das pessoas autoras.
+
+const expectedResult = [ 'Fundação', 'Duna' ];
+
+const booksByAuthorBirthYear = (birthYear) => {
+  return books
+  .filter((book) => book.author.birthYear === birthYear)
+  .map((book) => book.name);
+}
+console.log(booksByAuthorBirthYear(1920));
