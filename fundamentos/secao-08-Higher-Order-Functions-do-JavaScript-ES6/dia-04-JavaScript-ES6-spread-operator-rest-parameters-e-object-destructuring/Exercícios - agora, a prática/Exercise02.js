e58286c7e37f2283b17e4a44014610c98e51ba97
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-02-02 18:57:35 -0300

    Feito exercício 02 do tópico Exercícios - agora, a prática

--- fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-JavaScript-ES6-spread-operator-rest-parameters-e-object-destructuring/Exercícios - agora, a prática/Exercise02.js
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-04-JavaScript-ES6-spread-operator-rest-parameters-e-object-destructuring/Exercícios - agora, a prática/Exercise02.js
@@ -1,3 +1,9 @@
 // Crie uma função sum que, dado um número ilimitado de parâmetros, retorna a soma desses parâmetros. Ao chamar a função dessa forma : sum(4,5,6), o número 15 deve ser retornado.
 
 // escreva sum abaixo
+
+const sum = (...args) => {
+    return args.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sum(4, 5, 6))
