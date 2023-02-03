e038642722b212c87fa511a898118565b2cabb09
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-02-03 17:33:28 -0300

    Feito exercício 03 do tópico Exercícios - agora, a prática

--- fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-Array-destructuring-Object-property-shorthand-e-default-parameters-aberto-item-de-menu-com-subitens/Exercícios - agora, a prática/Exercise03.js
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-Array-destructuring-Object-property-shorthand-e-default-parameters-aberto-item-de-menu-com-subitens/Exercícios - agora, a prática/Exercise03.js
@@ -1,6 +1,9 @@
 // Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento. Um parâmetro adicional pode ser passado para alterar o cumprimento utilizado:
 
 // escreva greet abaixo
+const greet = (name, msg = 'Hi') => {
+    return `${msg} ${name}`;
+}
 
 // Retornos esperados:
 console.log(greet('John')); // 'Hi John'
