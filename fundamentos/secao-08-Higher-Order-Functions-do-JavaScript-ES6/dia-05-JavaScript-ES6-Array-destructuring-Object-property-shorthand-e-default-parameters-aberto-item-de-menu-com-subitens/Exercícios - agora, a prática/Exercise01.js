aa1c9d8431e88a9d12697f30aa969422ad49447a
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-02-03 14:59:13 -0300

    Feito exercício 01 do tópico Exercícios - agora, a prática

---
+++ fundamentos/secao-08-Higher-Order-Functions-do-JavaScript-ES6/dia-05-JavaScript-ES6-Array-destructuring-Object-property-shorthand-e-default-parameters-aberto-item-de-menu-com-subitens/Exercícios - agora, a prática/Exercise01.js
@@ -0,0 +1,10 @@
+// Escreva a função swap, que, dado um array de 3 elementos, retorna um novo array com o primeiro e terceiro elementos trocados. Detalhe: você precisa fazer essa função gastando apenas 1 linha. O retorno da função utilizando o array myList deve ser [3, 2, 5].
+
+const myList = [5, 2, 3];
+
+// escreva swap abaixo
+const swap = ([firstNumber, secondNumber, thirdNumber]) => {
+    return [firstNumber, secondNumber, thirdNumber] = [thirdNumber, secondNumber, firstNumber]
+}
+
+console.log(swap(myList));
