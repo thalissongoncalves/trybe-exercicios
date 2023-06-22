bf30adf6c174682dfc73197da0f8b864318871c8
Thálisson Gonçalves <thalissongsilva70@gmail.com>
2023-06-22 18:31:51 -0300

    Feito exercicio pratica - exercicio 02

--- back-end/secao-04-Introducao-ao-desenvolvimento-Web-com-Nodejs/dia-04-Nodejs-Express-e-Middlewares/exercicios-pratica/src/app.js
+++ back-end/secao-04-Introducao-ao-desenvolvimento-Web-com-Nodejs/dia-04-Nodejs-Express-e-Middlewares/exercicios-pratica/src/app.js
@@ -5,6 +5,8 @@ const verifyDescription = require('./middlewares/verifyDescription');
 const verifyCreatedAt = require('./middlewares/verifyCreatedAt');
 const verifyRating = require('./middlewares/verifyRating');
 const verifyDifficulty = require('./middlewares/verifyDifficulty');
+const generateToken = require('./utils/randomToken');
+const auth = require('./middlewares/auth');
 
 const app = express();
 
@@ -17,8 +19,21 @@ app.post('/activities',
   verifyCreatedAt,
   verifyRating,
   verifyDifficulty,
+  auth,
   (_req, res) => {
     res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
   });
+
+app.post('/signup', (req, res) => {
+  const { email, password, firstName, phone } = req.body;
+
+  if ([email, password, firstName, phone].includes(undefined)) {
+    return res.status(401).json({ message: 'Campos ausentes!' });
+  }
+
+  const token = generateToken();
+
+  return res.status(200).json({ token });
+});
 
 module.exports = app;
