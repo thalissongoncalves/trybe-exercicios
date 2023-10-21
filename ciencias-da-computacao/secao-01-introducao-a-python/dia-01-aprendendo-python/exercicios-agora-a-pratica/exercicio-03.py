52f73818221cff17e0a1e45fae20c99eb160930e
Thalisson Goncalves da Silva <thalissongsilva70@gmail.com>
2023-10-21 13:03:00 -0300

    Feito exercício 03 - Exercícios agora a prática

---
+++ ciencias-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/exercicios-agora-a-pratica/exercicio-03.py
@@ -0,0 +1,10 @@
+# Faça um programa que, dado um valor n qualquer, tal que n > 1, imprima na tela um quadrado feito de asteriscos de lado de tamanho n:
+
+def square(number):
+    print(number*'*')
+    print(number*'*')
+    print(number*'*')
+    print(number*'*')
+    print(number*'*')
+
+square(10)
