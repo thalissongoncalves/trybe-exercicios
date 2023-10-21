db728879db45a59d13d77d99e0db5051744cb282
Thalisson Goncalves da Silva <thalissongsilva70@gmail.com>
2023-10-21 13:00:57 -0300

    Feito exercício 01 - Exercícios agora a prática

---
+++ ciencias-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/exercicios-agora-a-pratica/exercicio-01.py
@@ -0,0 +1,9 @@
+# 🚀 Exercício 1: Crie uma função que receba dois números e retorne o maior deles.
+
+def function(intA, intB):
+    if intA > intB:
+        return intA
+    else:
+        return intB
+    
+print(function(1, 5))
