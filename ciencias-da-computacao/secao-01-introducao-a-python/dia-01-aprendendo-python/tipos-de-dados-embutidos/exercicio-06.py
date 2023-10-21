495e50856af62ceb9a485025dd17702c47a2ddb4
Thalisson Goncalves da Silva <thalissongsilva70@gmail.com>
2023-10-21 12:58:45 -0300

    Feito exercício 06 - Tipos de dados embutidos

---
+++ ciencias-da-computacao/secao-01-introducao-a-python/dia-01-aprendendo-python/tipos-de-dados-embutidos/exercicio-06.py
@@ -0,0 +1,16 @@
+# Realizar a contagem de quantas vezes cada elemento aparece em uma sequência é uma técnica muito útil na solução de alguns problemas. Qual é a estrutura mais recomendada para o armazenamento desta contagem?
+
+print("A estrutura mais recomendada para armazenar a contagem da frequência que um elemento aparece em uma sequência é o Dict. Veja abaixo uma possível solução para essa questão:")
+
+my_array = [20, 20, 1, 2]
+
+freq_dict = {}
+
+for item in my_array:
+    if (item in freq_dict):
+        freq_dict[item] += 1
+    else:
+        freq_dict[item] = 1
+
+for key, valor in freq_dict.items():
+    print(f"{key} : {valor}")
