8dcc512cf2da536ba4168e43bab25502dfa4d4de
Thalisson Goncalves da Silva <thalissongsilva70@gmail.com>
2023-10-22 14:37:03 -0300

    Feito exercício 01 - Entrada e saída

---
+++ ciencias-da-computacao/secao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-com-teste/entrada-e-saida/somador-de-numeros.py
@@ -0,0 +1,6 @@
+meu_numero = 0
+
+while meu_numero < 42:
+    meu_numero += int(input("Digite seu nũmero:\n"))
+
+print("A soma de seus números superou 42!")
---
+++ ciencias-da-computacao/secao-01-introducao-a-python/dia-02-entrada-e-saida-de-dados-com-teste/entrada-e-saida/teste.py
@@ -0,0 +1,7 @@
+print("Maria", "João", "Miguel", "Ana")
+print("Maria", "João", "Miguel", "Ana", sep=", ")
+
+import sys
+
+err = "Arquivo não encontrado"
+print(f"Erro aconteceu: {err}", file=sys.stderr)
