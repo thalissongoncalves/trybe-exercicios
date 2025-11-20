ef8b19800a0888f7f63e2e06ea483ca92ad7bd87
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-11-20 05:54:34 -0300

    Feito exercício 02 do dia

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicios/exercise-02.js
@@ -0,0 +1,9 @@
+let num1 = 25;
+let num2 = 30;
+
+if (num1 != num2) {
+    num1 > num2 ? console.log(num1 + " É o maior número") : console.log(num2 + " É o maior número");
+} else {
+    console.log("Os dois números são iguais.")
+}
+
