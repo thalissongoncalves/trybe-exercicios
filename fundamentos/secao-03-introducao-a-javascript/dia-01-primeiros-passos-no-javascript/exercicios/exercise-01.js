80d8127961d936b381724821fa9fd90fc469cefe
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-11-20 05:29:28 -0300

    Feito exercício 01 do dia

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/exercicios/exercise-01.js
@@ -0,0 +1,8 @@
+let num1 = 10;
+let num2 = 10;
+
+console.log(num1 + num2); // adição
+console.log(num1 - num2); // subtração
+console.log(num1 * num2); // multiplicação
+console.log(num1 / num2); // divisão
+console.log(num1 % num2); // módulo
