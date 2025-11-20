13eb98af148af5475cb952f4ce255bd933fee898
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-11-20 04:47:58 -0300

    Iniciado a seção 03, dia 01, feito a primeira atividade do dia

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/aulas/atividades/atividade-01.js
@@ -0,0 +1,7 @@
+const myName = "Thálisson";
+const birthCity = "Varginha";
+const birthYear = 2002;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
---
+++ fundamentos/secao-03-introducao-a-javascript/dia-01-primeiros-passos-no-javascript/aulas/variables.js
@@ -0,0 +1,11 @@
+let age = 20; // number
+let fullName = 'Lindomar'; //string
+let isValid = true; // boolean
+let xablau; // undefined
+let value = null; // null
+
+console.log(age);
+console.log(fullName);
+console.log(isValid);
+console.log(xablau);
+console.log(value);
