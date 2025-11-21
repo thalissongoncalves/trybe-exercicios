f0db99dfd3a3ceba54a97c7032401608f51dd0a6
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-11-21 19:51:14 -0300

    Feito anotações da aula de for

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/aulas/for.js
@@ -0,0 +1,5 @@
+const shopping = ["Sabão em pó", "Macarrão", "Leite Condensado", "Refrigerante", "Maçã", "Alface", "Nescau"];
+
+for (index = 0; index < shopping.length; index += 1) {
+  console.log(`${index} - ${shopping[index]}`)
+}
