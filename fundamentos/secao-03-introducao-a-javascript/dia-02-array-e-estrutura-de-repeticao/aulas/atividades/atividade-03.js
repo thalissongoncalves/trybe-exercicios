4e0eaa5045e79d704d86b08bb9749ee83c6c0769
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-11-21 19:52:26 -0300

    Feito atividade da aula de for

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-02-array-e-estrutura-de-repeticao/aulas/atividades/atividade-03.js
@@ -0,0 +1,7 @@
+// Utilize o for para imprimir os elementos da lista groceryList através de console.log():
+
+const groceryList = ["Arroz", "Feijão", "Alface", "Melancia", "Macarrão"];
+
+for (index = 0; index < groceryList.length; index += 1) {
+  console.log(groceryList[index]);
+}
