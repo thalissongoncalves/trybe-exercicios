8a29d0a90b973907e483d092bac0582b7ba0edc8
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-11-26 04:54:55 -0300

    Feito desafio 01 do projeto Playground Functions

---
+++ fundamentos/secao-03-introducao-a-javascript/dia-05-json/playground-functions-projet/challenges.js
@@ -0,0 +1,22 @@
+// Desafio 1 - Crie a função compareTrue
+const compareTrue = (boolean1, boolean2) =>
+  boolean1 && boolean2 ? true : false;
+console.log(compareTrue(true, true));
+
+// Desafio 2 - Crie a função splitSentence
+
+// Desafio 3 - Crie a função concatName
+
+// Desafio 4 - Crie a função footballPoints
+
+// Desafio 5 - Crie a função highestCount
+
+// Desafio 6 - Crie as funções calcTriangleArea, calcRectangleArea e calcAllAreas
+
+// Desafio 7 - Crie a função catAndMouse
+
+// Desafio 8 - Crie a função fizzBuzz
+
+// Desafio 9 - Crie a função encode e a função decode
+
+// Desafio 10 - Crie a função techList
