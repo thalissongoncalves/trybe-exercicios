09be3dc8ac4973ae727a01031c01fa707fb06a37
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-18 17:18:53 -0300

    Feito aula 01, 02 e 03 do dia 01 seção 02 backend

---
+++ backend/secao-02-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/aulas/02-importando-modulos/app/index.js
@@ -0,0 +1,9 @@
+const {
+  celsiusToFahrenheit,
+  fahrenheitToCelsius,
+} = require("../mymodules/temp"); // Módulo local
+
+const path = require("path"); // Módulo interno
+
+console.log(celsiusToFahrenheit(100));
+console.log(fahrenheitToCelsius(212));
