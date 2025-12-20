1bb759b8e3dfcf5c7d49b2e157b5de9c2699aca1
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-20 20:23:51 -0300

    Feito exercício 04 do dia 04 seção 04

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/dia-04-web-storage/exercicios/script.js
@@ -1,77 +1,80 @@
 window.onload = () => {
   const setBackgroundColor = (color) => {
-    let content = document.querySelector(".content")
-    content.style.backgroundColor = color
-    localStorage.setItem('backgroundColor', color);
-  }
+    let content = document.querySelector(".content");
+    content.style.backgroundColor = color;
+    localStorage.setItem("backgroundColor", color);
+  };
 
   const setFontColor = (color) => {
-    let paragraphs = document.querySelectorAll(".paragraph")
+    let paragraphs = document.querySelectorAll(".paragraph");
     for (let index = 0; index < paragraphs.length; index += 1) {
-      paragraphs[index].style.color = color
+      paragraphs[index].style.color = color;
     }
-    localStorage.setItem('color', color);
-  }
+    localStorage.setItem("color", color);
+  };
 
   const setFontSize = (size) => {
-    let paragraphs = document.querySelectorAll(".paragraph")
+    let paragraphs = document.querySelectorAll(".paragraph");
     for (let index = 0; index < paragraphs.length; index += 1) {
-      paragraphs[index].style.fontSize = size
+      paragraphs[index].style.fontSize = size;
     }
-    localStorage.setItem('fontSize', size);
-  }
+    localStorage.setItem("fontSize", size);
+  };
 
   const setLineHeight = (height) => {
-    let paragraphs = document.querySelectorAll(".paragraph")
+    let paragraphs = document.querySelectorAll(".paragraph");
     for (let index = 0; index < paragraphs.length; index += 1) {
-      paragraphs[index].style.lineHeight = height
+      paragraphs[index].style.lineHeight = height;
     }
-  }
+    localStorage.setItem("lineHeight", height);
+  };
 
   const setFontFamily = (family) => {
-    let paragraphs = document.querySelectorAll(".paragraph")
+    let paragraphs = document.querySelectorAll(".paragraph");
     for (let index = 0; index < paragraphs.length; index += 1) {
-      paragraphs[index].style.fontFamily = family
+      paragraphs[index].style.fontFamily = family;
     }
-  }
+  };
 
   // Cor de fundo da tela;
-  let backgroundColorButtons = document.querySelectorAll("#background-color>button")
+  let backgroundColorButtons = document.querySelectorAll(
+    "#background-color>button"
+  );
   for (let index = 0; index < backgroundColorButtons.length; index += 1) {
     backgroundColorButtons[index].addEventListener("click", (event) => {
-      setBackgroundColor(event.target.innerHTML)
-    })
+      setBackgroundColor(event.target.innerHTML);
+    });
   }
 
   // Cor do texto;
-  let fontColorButtons = document.querySelectorAll("#font-color>button")
+  let fontColorButtons = document.querySelectorAll("#font-color>button");
   for (let index = 0; index < fontColorButtons.length; index += 1) {
     fontColorButtons[index].addEventListener("click", (event) => {
-      setFontColor(event.target.innerHTML)
-    })
+      setFontColor(event.target.innerHTML);
+    });
   }
 
   // Tamanho da fonte
-  let fontSizeButtons = document.querySelectorAll("#font-size>button")
+  let fontSizeButtons = document.querySelectorAll("#font-size>button");
   for (let index = 0; index < fontSizeButtons.length; index += 1) {
     fontSizeButtons[index].addEventListener("click", (event) => {
-      setFontSize(event.target.innerHTML)
-    })
+      setFontSize(event.target.innerHTML);
+    });
   }
 
   // Espaçamento entre as linhas do texto;
-  let lineHeightButtons = document.querySelectorAll("#line-height>button")
+  let lineHeightButtons = document.querySelectorAll("#line-height>button");
   for (let index = 0; index < lineHeightButtons.length; index += 1) {
     lineHeightButtons[index].addEventListener("click", (event) => {
-      setLineHeight(event.target.innerHTML)
-    })
+      setLineHeight(event.target.innerHTML);
+    });
   }
 
   // Tipo da fonte (_Font family_).
-  let fontFamilyButtons = document.querySelectorAll("#font-family>button")
+  let fontFamilyButtons = document.querySelectorAll("#font-family>button");
   for (let index = 0; index < fontFamilyButtons.length; index += 1) {
     fontFamilyButtons[index].addEventListener("click", (event) => {
-      setFontFamily(event.target.innerHTML)
-    })
+      setFontFamily(event.target.innerHTML);
+    });
   }
-}
+};
