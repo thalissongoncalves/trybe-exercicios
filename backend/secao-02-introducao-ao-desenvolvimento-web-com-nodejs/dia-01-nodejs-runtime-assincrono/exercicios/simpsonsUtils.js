c88a8f5343c9fa303cc8092ff31b7b597ca29c9d
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-22 16:55:49 -0300

    Feito exercício 05 - A

---
+++ backend/secao-02-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/exercicios/simpsonsUtils.js
@@ -0,0 +1,16 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const SIMPSONS_DATA_PATH = "./simpsons.json";
+
+async function readSimpsonsData() {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, SIMPSONS_DATA_PATH));
+    const simpsons = JSON.parse(data);
+    for (i = 0; i < simpsons.length; i += 1) {
+      console.log(`${simpsons[i].id} - ${simpsons[i].name}`);
+    }
+  } catch (err) {
+    console.error(`Erro na leitura do arquivo: ${err}`);
+  }
+}
