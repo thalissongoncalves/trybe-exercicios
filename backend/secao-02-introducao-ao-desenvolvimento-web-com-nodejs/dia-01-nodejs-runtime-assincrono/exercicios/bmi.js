65cb3d784bd9ec097b063d8e6a486260553b1432
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-22 16:21:13 -0300

    Feito exercício 04

--- backend/secao-02-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/exercicios/bmi.js
+++ backend/secao-02-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/exercicios/bmi.js
@@ -3,7 +3,20 @@ const readline = require("readline-sync");
 const imc = () => {
   const peso = readline.questionInt("What's your weight? ");
   const altura = readline.questionFloat("What's your height? (Ex: 1.8) = ");
-  return peso / altura ** 2;
+  const imcValue = peso / altura ** 2;
+  if (imcValue < 18.5) {
+    return `IMC: ${imcValue} | Situação: Abaixo do peso (magreza).`;
+  } else if (imcValue >= 18.5 && imcValue <= 24.9) {
+    return `IMC: ${imcValue} | Situação: Peso normal.`;
+  } else if (imcValue >= 25 && imcValue <= 29.9) {
+    return `IMC: ${imcValue} | Situação: Acima do peso (sobrepeso).`;
+  } else if (imcValue >= 30 && imcValue <= 34.9) {
+    return `IMC: ${imcValue} | Situação: Obesidade grau I.`;
+  } else if (imcValue >= 35 && imcValue <= 39.9) {
+    return `IMC: ${imcValue} | Situação: Obesidade grau II.`;
+  } else {
+    return `IMC: ${imcValue} | Situação: Obesidade grau III e IV.`;
+  }
 };
 
 console.log(imc());
