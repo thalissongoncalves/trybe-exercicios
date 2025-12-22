599679f9239ecee4a91ddc8518fc5737fb594176
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-22 15:58:44 -0300

    Feito aula e anotações do dia 01 - seção 02

---
+++ backend/secao-02-introducao-ao-desenvolvimento-web-com-nodejs/dia-01-nodejs-runtime-assincrono/aulas/06-leitura-e-escrita-de-arquivos/annotations.js
@@ -0,0 +1,11 @@
+// fs.readFileSync
+// - Síncrono
+// - Não usa callback nem promise
+
+// fs.readFile
+// - Assíncrono
+// - Usa callback
+
+// fs.promises.readFile
+// - Assíncrono
+// - Usa promises - ou seja async/await ou then/catch
