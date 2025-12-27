98b12b21d2a22b1e517aef85680636e47fd337c2
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-26 23:49:12 -0300

    Feito requisito 05 - Implemente uma função usando localStorage para que a paleta de cores gerada aleatoriamente seja mantida após recarregar a página

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/project-pixels-art/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/project-pixels-art/script.js
@@ -1,5 +1,6 @@
 const colorPallets = document.getElementsByClassName('color');
 const buttonRandomColor = document.getElementById('button-random-color');
+const colorPaletteStorage = localStorage.getItem('colorPalette');
 
 function mudarCorAleatoria(element) {
   let r = Math.floor(Math.random() * 256);
@@ -14,22 +15,46 @@ function mudarCorAleatoria(element) {
 
   const corAleatoria = `rgb(${r}, ${g}, ${b})`;
   element.style.backgroundColor = corAleatoria;
+
+  return corAleatoria;
 }
 
-for (i = 0; i < colorPallets.length; i += 1) {
-  if (i == 0) {
-    colorPallets[0].style.backgroundColor = "black";
-  } else {
-    mudarCorAleatoria(colorPallets[i]);
+function changeColors() {
+  const colors = [];
+
+  for (i = 0; i < colorPallets.length; i += 1) {
+    if (i == 0) {
+      colorPallets[0].style.backgroundColor = 'black';
+      colors.push('black');
+    } else {
+      const colorResult = mudarCorAleatoria(colorPallets[i], i);
+      colors.push(colorResult);
+    }
   }
+
+  localStorage.setItem('colorPalette', JSON.stringify(colors));
 }
 
-buttonRandomColor.addEventListener('click', () => {
+buttonRandomColor.addEventListener('click', changeColors);
+
+if (!colorPaletteStorage) {
+  changeColors();
+  const colors = [];
+
+  for (i = 0; i < colorPallets.length; i += 1) {
+    colors.push(colorPallets[i].style.backgroundColor);
+  }
+
+  localStorage.setItem('colorPalette', JSON.stringify(colors));
+  console.log(colors);
+} else {
+  const colors = JSON.parse(colorPaletteStorage);
+
   for (i = 0; i < colorPallets.length; i += 1) {
     if (i == 0) {
       colorPallets[0].style.backgroundColor = 'black';
     } else {
-      mudarCorAleatoria(colorPallets[i]);
+      colorPallets[i].style.backgroundColor = colors[i];
     }
   }
-});
+}
