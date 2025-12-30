c3f32332b9c7b14ee7ea8c9c2696f901db592aa5
Thálisson Gonçalves <thalissongdev@gmail.com>
2025-12-30 04:06:13 -0300

    Feito requisito 15 - Crie uma função para manter o tamanho novo do board ao recarregar a página

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/project-pixels-art/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/project-pixels-art/script.js
@@ -1,27 +1,30 @@
-const colorPallets = document.getElementsByClassName('color');
-const buttonRandomColor = document.getElementById('button-random-color');
-const colorPaletteStorage = localStorage.getItem('colorPalette');
-const pixelBoardElement = document.getElementById('pixel-board');
-const clearBtn = document.getElementById('clear-board');
+const colorPallets = document.getElementsByClassName("color");
+const buttonRandomColor = document.getElementById("button-random-color");
+const colorPaletteStorage = localStorage.getItem("colorPalette");
+const pixelBoardElement = document.getElementById("pixel-board");
+const clearBtn = document.getElementById("clear-board");
 const pixelBoardElementsStorage = JSON.parse(
-  localStorage.getItem('pixelBoard')
+  localStorage.getItem("pixelBoard")
 );
-const gridColumnsStorage = localStorage.getItem('gridColumns');
-const setPixelBtn = document.getElementById('generate-board');
-const setPixelInput = document.getElementById('board-size');
+const gridColumnsStorage = localStorage.getItem("boardSize");
+const setPixelBtn = document.getElementById("generate-board");
+const setPixelInput = document.getElementById("board-size");
 let pixelsElementsDefault = 25;
 
 if (!gridColumnsStorage) {
-  localStorage.setItem('gridColumns', 'auto auto auto auto auto');
-  pixelBoardElement.style.gridTemplateColumns =
-    localStorage.getItem('gridColumns');
+  localStorage.setItem("boardSize", 5);
+  localStorage.setItem("gridColumn", "auto auto auto auto auto");
+  pixelBoardElement.style.gridTemplateColumns = "auto auto auto auto auto";
 } else {
-  pixelBoardElement.style.gridTemplateColumns =
-    localStorage.getItem('gridColumns');
+  let boardSize = "";
+  for (index = 0; index < localStorage.getItem("boardSize"); index += 1) {
+    boardSize += "auto ";
+  }
+  pixelBoardElement.style.gridTemplateColumns = boardSize;
 }
 
-localStorage.setItem('colorSelected', 'black');
-colorPallets[0].classList.add('selected');
+localStorage.setItem("colorSelected", "black");
+colorPallets[0].classList.add("selected");
 let pixelElementsArray = [];
 
 function mudarCorAleatoria(element) {
@@ -46,25 +49,25 @@ function changeColors() {
 
   for (i = 0; i < colorPallets.length; i += 1) {
     if (i == 0) {
-      colorPallets[0].style.backgroundColor = 'black';
-      colorPallets[0].classList.add('selected');
-      colors.push('black');
+      colorPallets[0].style.backgroundColor = "black";
+      colorPallets[0].classList.add("selected");
+      colors.push("black");
     } else {
       const colorResult = mudarCorAleatoria(colorPallets[i], i);
       colors.push(colorResult);
     }
   }
 
-  localStorage.setItem('colorPalette', JSON.stringify(colors));
+  localStorage.setItem("colorPalette", JSON.stringify(colors));
 }
 
 function cleanSelected() {
   for (i = 0; i < colorPallets.length; i += 1) {
-    colorPallets[i].classList.remove('selected');
+    colorPallets[i].classList.remove("selected");
   }
 }
 
-buttonRandomColor.addEventListener('click', changeColors);
+buttonRandomColor.addEventListener("click", changeColors);
 
 if (!colorPaletteStorage) {
   changeColors();
@@ -74,14 +77,13 @@ if (!colorPaletteStorage) {
     colors.push(colorPallets[i].style.backgroundColor);
   }
 
-  localStorage.setItem('colorPalette', JSON.stringify(colors));
-  console.log(colors);
+  localStorage.setItem("colorPalette", JSON.stringify(colors));
 } else {
   const colors = JSON.parse(colorPaletteStorage);
 
   for (i = 0; i < colorPallets.length; i += 1) {
     if (i == 0) {
-      colorPallets[0].style.backgroundColor = 'black';
+      colorPallets[0].style.backgroundColor = "black";
     } else {
       colorPallets[i].style.backgroundColor = colors[i];
     }
@@ -90,17 +92,17 @@ if (!colorPaletteStorage) {
 
 if (!pixelBoardElementsStorage) {
   for (i = 0; i < 25; i += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixel.style.backgroundColor = 'white';
+    const pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.backgroundColor = "white";
     pixel.dataset.index = i;
     pixelBoardElement.appendChild(pixel);
     pixelElementsArray.push(pixel.style.backgroundColor);
   }
 } else {
   for (i = 0; i < pixelBoardElementsStorage.length; i += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
+    const pixel = document.createElement("div");
+    pixel.className = "pixel";
     pixel.style.backgroundColor = pixelBoardElementsStorage[i];
     pixel.dataset.index = i;
     pixelBoardElement.appendChild(pixel);
@@ -108,37 +110,39 @@ if (!pixelBoardElementsStorage) {
   }
 }
 
-localStorage.setItem('pixelBoard', JSON.stringify(pixelElementsArray));
+localStorage.setItem("pixelBoard", JSON.stringify(pixelElementsArray));
 
 for (i = 0; i < colorPallets.length; i += 1) {
-  colorPallets[i].addEventListener('click', (e) => {
+  colorPallets[i].addEventListener("click", (e) => {
     cleanSelected();
-    e.target.classList.add('selected');
-    localStorage.setItem('colorSelected', e.target.style.backgroundColor);
+    e.target.classList.add("selected");
+    localStorage.setItem("colorSelected", e.target.style.backgroundColor);
   });
 }
 
-const pixelElements = document.getElementsByClassName('pixel');
+const pixelElements = document.getElementsByClassName("pixel");
 
 for (i = 0; i < pixelElements.length; i += 1) {
-  pixelElements[i].addEventListener('click', (e) => {
-    const colorSelected = localStorage.getItem('colorSelected');
-    const pixelBoardElements = JSON.parse(localStorage.getItem('pixelBoard'));
+  pixelElements[i].addEventListener("click", (e) => {
+    const colorSelected = localStorage.getItem("colorSelected");
+    const pixelBoardElements = JSON.parse(localStorage.getItem("pixelBoard"));
     e.target.style.backgroundColor = colorSelected;
     pixelBoardElements[e.target.dataset.index] = colorSelected;
-    localStorage.setItem('pixelBoard', JSON.stringify(pixelBoardElements));
+    localStorage.setItem("pixelBoard", JSON.stringify(pixelBoardElements));
   });
 }
 
-clearBtn.addEventListener('click', () => {
+clearBtn.addEventListener("click", () => {
   for (i = 0; i < pixelElements.length; i += 1) {
-    pixelElements[i].style.backgroundColor = 'white';
+    pixelElements[i].style.backgroundColor = "white";
   }
 });
 
-setPixelBtn.addEventListener('click', () => {
+setPixelBtn.addEventListener("click", () => {
+  let boardSizeString = "";
+
   if (!setPixelInput.value) {
-    alert('Board inválido!');
+    alert("Board inválido!");
   }
 
   if (setPixelInput.value < 5) {
@@ -155,22 +159,23 @@ setPixelBtn.addEventListener('click', () => {
     pixelBoardElement.removeChild(pixelBoardElement.firstChild);
   }
 
-  for (index02 = 0; index02 < pixelsElementsDefault; index02 += 1) {
-    const pixel = document.createElement('div');
-    pixel.className = 'pixel';
-    pixel.style.backgroundColor = 'white';
-    pixel.dataset.index = index02;
+  for (index = 0; index < pixelsElementsDefault; index += 1) {
+    const pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.backgroundColor = "white";
+    pixel.dataset.index = index;
     pixelBoardElement.appendChild(pixel);
     pixelElementsArray.push(pixel.style.backgroundColor);
   }
 
-  let gridTemplateColumns = '';
-
-  for (index03 = 0; index03 < setPixelInput.value; index03 += 1) {
-    gridTemplateColumns += 'auto ';
-  }
+  let gridTemplateColumns = setPixelInput.value;
 
   pixelBoardElement.style.gridTemplateColumns = gridTemplateColumns;
-  localStorage.setItem('pixelBoard', JSON.stringify(pixelElementsArray));
-  localStorage.setItem('gridColumns', gridTemplateColumns);
+  localStorage.setItem("pixelBoard", JSON.stringify(pixelElementsArray));
+  localStorage.setItem("boardSize", gridTemplateColumns);
+  for (index02 = 0; index02 < localStorage.getItem("boardSize"); index02 += 1) {
+    boardSizeString += "auto ";
+  }
+  pixelBoardElement.style.gridTemplateColumns = boardSizeString;
+  localStorage.setItem("gridColumn", boardSizeString);
 });
