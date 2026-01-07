2194df30e8ccd90c00eb39260bb00a76ffc1419d
Thálisson Gonçalves <thalissongdev@gmail.com>
2026-01-07 16:11:45 -0300

    Feito requisito 07 - Permita que um item da lista ao ser clicado tenha sua cor de fundo alterada para cinza

--- fundamentos/secao-04-javascript-dom-eventos-e-web-storage/project-todo-list/script.js
+++ fundamentos/secao-04-javascript-dom-eventos-e-web-storage/project-todo-list/script.js
@@ -1,10 +1,14 @@
-const input = document.getElementById("texto-tarefa");
-const taskList = document.getElementById("lista-tarefas");
-const addTaskBtn = document.getElementById("criar-tarefa");
+const input = document.getElementById('texto-tarefa');
+const taskList = document.getElementById('lista-tarefas');
+const addTaskBtn = document.getElementById('criar-tarefa');
 
-addTaskBtn.addEventListener("click", () => {
-  const listItem = document.createElement("li");
+addTaskBtn.addEventListener('click', () => {
+  const listItem = document.createElement('li');
   listItem.innerText = input.value;
   taskList.appendChild(listItem);
-  input.value = "";
+  input.value = '';
+  listItem.addEventListener(
+    'click',
+    (e) => (e.target.style.backgroundColor = 'rgb(128, 128, 128)')
+  );
 });
